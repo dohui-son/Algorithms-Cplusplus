9c48d15653ed067b6e0e391633f8f9b71be81079
dohui-son <thsehgml37@korea.ac.kr>
2021-06-24 23:48:00 +0900

    chore : mac setting

---
+++ foundation/dfs.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int visited[1004][1004];
+void dfs(int y, int x)
+{
+    visited[y][x] = 1;
+    //for(int i=0; i)
+}
+
+int main()
+{
+    return 0;
+}
