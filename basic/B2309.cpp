01ea448286cbec953790290c12c088e3b11b9438
dohui-son <thsehgml37@korea.ac.kr>
2021-06-29 18:55:51 +0900

    review

--- basic/B2309.cpp
+++ basic/B2309.cpp
@@ -1,38 +1,45 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define endl "\n";
 
-vector<int> v = vector<int>(9, 0);
-int s;
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    for (int i = 0; i < 9; i++)
-    {
-        cin >> v[i];
-        s += v[i];
-    }
-    for (int i = 0; i < 9; i++)
-    {
-        rotate(v.begin(), v.begin() + 1, v.end());
-        s -= v[7];
-        s -= v[8];
-        cout << "s " << s << endl;
-        if (s == 100)
-            break;
-        s += v[7];
-        s += v[8];
-    }
-    v.pop_back();
-    v.pop_back();
-    sort(v.begin(), v.end());
 
-    for (int i = 0; i < 7; i++)
-    {
-        cout << v[i] << endl;
-    }
+//dohui's solution
 
-    return 0;
-}
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define endl "\n";
+
+// vector<int> v = vector<int>(9, 0);
+// int s;
+// int main()
+// {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     cout.tie(nullptr);
+//     for (int i = 0; i < 9; i++)
+//     {
+//         cin >> v[i];
+//         s += v[i];
+//     }
+//     for (int i = 0; i < 9; i++)
+//     {
+//         for (int j = i + 1; j < 9; j++)
+//         {
+//             s -= v[i];
+//             s -= v[j];
+//             if (s == 100)
+//             {
+//                 v[i] = 0;
+//                 v[j] = 0;
+//                 break;
+//             }
+//             s += v[i];
+//             s += v[j];
+//         }
+//     }
+//     sort(v.begin(), v.end());
+
+//     for (int i = 2; i < 9; i++)
+//     {
+//         cout << v[i] << endl;
+//     }
+
+//     return 0;
+// }
