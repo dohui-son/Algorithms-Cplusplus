9e487829c2bcfe5608173bf4417673c915db5447
dohui-son <thsehgml37@korea.ac.kr>
2021-07-11 22:12:37 +0900

    2 pointers - important sol

--- twopointers/B13144.cpp
+++ twopointers/B13144.cpp
@@ -1,12 +1,39 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
-
 #define endl "\n";
+long long s, e, cnt[100001], n, a[100001], ret;
 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-int main(){
-    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
 
+    while (e < n)
+    {
+        if (!cnt[a[e]])
+        { //지금 살펴본 애는 중복 안됨 (이때까지 동일한 애 뽑은 적 없음)
+            cnt[a[e]]++;
+            e++;
+        }
+        else
+        { //이번애가 중복됨 (중복 발견)
+            ret += (e - s);
+            cnt[a[s]]--;
+            s++;
+        }
+    }
+    ret += (long long)(e - s) * (e - s + 1) / 2;
+    cout << ret;
 
     return 0;
 }
--- twopointers/B1644.cpp
+++ twopointers/B1644.cpp
@@ -1,1 +1,41 @@
-#include
+#include <iostream>
+#include <vector>
+
+using namespace std;
+#define endl "\n";
+int n, ans, p, high, low, sum;
+vector<int> prime;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cin >> n;
+    vector<bool> nat(n);
+    for (int i = 2; i <= n; i++)
+    {
+        if (nat[i])
+            continue; // "소수가 아닌"자연수 이다
+        for (int j = i * 2; j <= n; j += i)
+            nat[j] = 1;
+    }
+    for (int i = 2; i <= n; i++)
+        if (!nat[i])
+            prime.push_back(i);
+    int p = prime.size();
+    while (1)
+    {
+        if (sum >= n)
+            sum -= prime[low++];
+        else if (high == p)
+            break;
+        else
+            sum += prime[high++];
+
+        if (sum == n)
+            ans++;
+    }
+    cout << ans;
+    return 0;
+}
