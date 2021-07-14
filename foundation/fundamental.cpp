525b6b336b663b76d7692129588a19890acbd354
dohui-son <thsehgml37@korea.ac.kr>
2021-07-14 17:26:32 +0900

    review

--- foundation/fundamental.cpp
+++ foundation/fundamental.cpp
@@ -5,67 +5,142 @@
 #include <unordered_map>
 #include <tuple>
 #include <string>
+
 using namespace std;
 #define endl "\n";
 
-pair<int, int> p;
-tuple<int, int, int> tl();
-int a, b, c;
-vector<int> v;
-
 int main()
 {
-    p = make_pair(1, 2);
-    tl = make_tuple(1, 2, 3);
-    tie(a, b) = p;
-    tie(a, b, c) = tl;
+    char s[10];
+    string str = "wow fantastic";
+    str += " ";
+    str += "space added";
+    cout << str << endl;
+    str = ""; // string 초기화
+    cout << "str : " << str << endl;
 
-    for (int i = 0; i < 10; i++)
-        v.push_back(i);
-    for (int a : v)
-        cout << a << " ";
-    v.pop_back();
-    v.erase(v.begin(), v.begin() + 1);
-    auto a = find(v.begin(), v.end(), 100);
-    if (a == v.end())
+    // s[0] = {
+    //     0,
+    // }; --> 이거 안됨
+    fill(s, s + 10, 0);              //char vector 초기화
+    memset(s, 0, sizeof(char) * 10); //char vector 초기화
+    for (char a : s)
+    {
+        cout << a << endl;
+        cout << "char vector 초기화 완료";
+    }
+    cout << endl;
+
+    char c = '4';
+    int to_int = c - '0'; // 정수형 4
+    c = 'C';
+    to_int = c - 'A' + 10; // 정수형 12
+
+    c = '\0'; // char 형 초기화 --- null 인 \0 넣기
+    cout << "c : " << c << endl;
+    cout << "초기화" << endl;
+
+    str = "123";
+    str[0]++;
+    cout << str << endl; //223
+
+    // 입력 : 1293746 일때 입력받는 방법
+    vector<int> v(5, 0);
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%1d", &v[i]);
+        cout << "after scanf(\" % 1d \", &v[i]) : " << v[i] << endl;
+    }
+    getline(cin, str); // "안녕하세요 날씨가 좋네요" 입력받기
+
+    //***입력을 주다가 안줄 때 끝난다고 명시되어있을때***
+    while (scanf("%d", &to_int) != EOF)
+    {
+    }
+    while (cin >> to_int)
+    {
+    }
+
+    double a1 = 3.2;
+    double a2 = 3.7;
+    double a3 = -3.2;
+    double a4 = -3.7;
+
+    cout << "[C++] round 반올림 example" << endl;
+    cout << "round(3.2) : " << round(a1) << endl;
+    cout << "round(3.7) : " << round(a2) << endl;
+    cout << "round(-3.2) : " << round(a3) << endl; //round(-3.2) : -3
+    cout << "round(-3.7) : " << round(a4) << endl; //round(-3.7) : -4
+
+    //올림 함수
+    cout << "ceil(4.3) " << ceil(4.3) << endl;
+    cout << "ceil(-4.3) " << ceil(-4.3) << endl;
+    //내림 함수
+    cout << "floor(4.7) " << floor(4.7) << endl;
+    cout << "floor(-4.7) " << floor(-4.7) << endl;
+    //소수점 아래는 다 없애는 함수 c++11
+    cout << "trunc(-4.7) " << trunc(-4.7) << endl;
+    cout << "trunc(4.7) " << trunc(4.7) << endl;
+    //양수일때 ceil 값과 trunc 값이 다릅니다.
+    //음수일때 floor 값과 trunc 값이 다릅니다.
+
+    to_int = (int)c * 100;
+    to_int = (int)100 * c; //이렇게 하면 형변환 안됨
+
+    //asci code A-Z 65-90
+    //asci code a-z 97-122
+    //****a에서 z까지 입력받는데 이를 정수로 0-26으로 표현 *****
+    c = 'a';
+    cout << (int)c - 97 << endl;  //0
+    cout << (int)c - 'a' << endl; //0
+
+    str = "this is string";
+    cout << str.substr(0, 3) << endl; // 시작지점으로부터 몇개 문자열 뽑아내기
+    cout << str.substr(1, 3) << endl;
+    reverse(str.begin(), str.end());
+    cout << str << endl;
+    reverse(str.begin(), str.begin() + 6);
+    cout << str << endl;
+    auto au = find(str.begin(), str.end(), 'i');
+    cout << "*au to print     " << *au << endl;
+    if (au == str.end())
         cout << "not found" << endl;
+    str.erase(str.begin(), str.begin() + 2);
+    cout << str << endl;
 
-    int maxim = *max_element(v.begin(), v.end());
-    int mini = *min_element(v.begin(), v.end());
+    pair<int, int> pi;
+    pi = {1, 2};
+    int num1 = 0, num2 = 0, num3 = 0;
+    tie(num1, num2) = pi;
+    cout << num1 << "  " << num2 << endl;
+    tuple<int, int, int> tl;
+    tl = {1, 2, 3};
+    tie(num1, num2, num3) = tl;
+    cout << num1 << "  " << num2 << " " << num3 << endl;
 
-    v.clear();                    //배열 초기화
-    fill(v.begin(), v.end(), 10); //시작값, 끝값, 초기화하는 값   ==>vector에도 사용가능
-    //memset은 0, -1 으로만 초기화가 가능. 왜냐하면 memset이 byte로 초기화를 해버리기 때문
-    int b[10] = {
-        0,
-    };
-    int bb[10][10] = {{
-        0,
-    }};
-    fill(b, b + 10, 0);
-    //2차원 배열 초기화 : fill;
-    for (size_t i = 0; i < 10; i++)
-        fill(bb[i], bb[i] + 10, 0);
-    //fill(&bb[0][0], &bb[0][0] + n * m, k);
+    v.push_back(100);
+    v.pop_back();
+    for (int i : v)
+        cout << "v " << i << " ";
+    cout << endl;
+    v.erase(v.begin() + 2, v.begin() + 3);
+    for (int i : v)
+        cout << "v " << i << " ";
 
-    int arr[10] = {
-        0,
-    };
-    for (int a : arr)
-        cout << a << " ";
-    auto a = find(arr, arr + 10, 100);
-    if (a == arr + 10)
-        cout << "not found";
-
-    fill(arr, arr + 10, 10);
-
-    unordered_map<string, int> umap;
-    umap.insert({"test1", 1});
-    umap["test1"] = 4;
-    // for (auto element : umap)
-    // {
-    //     cout
-    // }
+    cout << endl;
+    auto aut = find(v.begin() + 2, v.end(), 7);
+    if (aut == v.end())
+    {
+        cout << "no 7" << endl;
+    }
+    else
+    {
+        cout << *aut << endl;
+    }
+    fill(v.begin(), v.end(), 10);
+    v.clear(); //사이즈를 0으로 만들어줌
+    for (int i : v)
+        cout << "v " << i << endl;
 
     return 0;
 }
--- src/B4949.cpp
+++ src/B4949.cpp
@@ -1,45 +1,45 @@
-//#include <iostream>
-//#include <string>
-//#include <stack>
-//#include <vector>
-//
-//using namespace std;
-//#define endl "\n"
-//string str;
-//vector<bool> ans;
-//
-//int main() {
-//	ios::sync_with_stdio(false);
-//	cin.tie(nullptr);
-//	cout.tie(nullptr);
-//
-//	while (getline(cin,str)) {
-//		if (str[0] == '.' && str.size() == 1)break;
-//
-//		stack<char> st;
-//		for (int i = 0; i < str.size(); i++)
-//		{
-//			if (str[i] == '(' || str[i] == '[')st.push(str[i]);
-//			if (str[i] == ')' || str[i] == ']') {
-//				if (st.empty()) {
-//					st.push(str[i]);
-//					break;
-//				}
-//				else {
-//					if (st.top() == '(' && str[i] ==')')st.pop();
-//					else if(st.top() == '[' && str[i] == ']')st.pop();
-//				}
-//			}
-//		}
-//		if (st.empty())ans.push_back(true);
-//		else ans.push_back(false);
-//	}
-//
-//	for (bool i : ans) {
-//		if (i)cout << "yes" << endl;
-//		else cout << "no" << endl;
-//	}
-//
-//	return 0;
-//
-//}
+#include <iostream>
+#include <string>
+#include <stack>
+#include <vector>
+
+using namespace std;
+#define endl "\n"
+string str;
+vector<bool> ans;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	while (getline(cin,str)) {
+		if (str[0] == '.' && str.size() == 1)break;
+
+		stack<char> st;
+		for (int i = 0; i < str.size(); i++)
+		{
+			if (str[i] == '(' || str[i] == '[')st.push(str[i]);
+			if (str[i] == ')' || str[i] == ']') {
+				if (st.empty()) {
+					st.push(str[i]);
+					break;
+				}
+				else {
+					if (st.top() == '(' && str[i] ==')')st.pop();
+					else if(st.top() == '[' && str[i] == ']')st.pop();
+				}
+			}
+		}
+		if (st.empty())ans.push_back(true);
+		else ans.push_back(false);
+	}
+
+	for (bool i : ans) {
+		if (i)cout << "yes" << endl;
+		else cout << "no" << endl;
+	}
+
+	return 0;
+
+}
