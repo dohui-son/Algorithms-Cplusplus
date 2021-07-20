2ae8b2154732f2bc883396cd4c393544ef19d4c2
dohui-son <thsehgml37@korea.ac.kr>
2021-07-20 23:38:53 +0900

    greedy

--- greedy/B2109.cpp
+++ greedy/B2109.cpp
@@ -1,35 +1,37 @@
-#include<bits/stdc++.h>
-//다시 한번 더 보기
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
 using namespace std;
+int n, d, p, ans;
+priority_queue<int, vector<int>, greater<int> > pq;
+vector<pair<int, int> > v;
+#define endl '\n';
 
-int n,d,p,ans;
-vector<pair<int,int>> v;
-priority_queue<int, vector<int>,greater<int>>pq;
-int main(){
-    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     cin >> n;
-    for(int i = 0 ; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
         cin >> p >> d;
-        v.push_back({d,p});
+        v.push_back({d, p});
     }
-    sort(v.begin(),v.end());
+    sort(v.begin(), v.end());
     for (int i = 0; i < n; i++)
     {
         pq.push(v[i].second);
-        if(pq.size() > v[i].first)pq.pop();
+        if (v[i].first < pq.size())
+            pq.pop();
     }
-
-    while(pq.size()){
-        ans+=pq.top(); pq.pop();
+    while (pq.size())
+    {
+        ans += pq.top();
+        pq.pop();
     }
-    
-
-    
-    cout << ans;
-
+    cout << ans << endl;
 
     return 0;
-    
-} 
+}
