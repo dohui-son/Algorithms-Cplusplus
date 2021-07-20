2af170bf38c3c29a4c70f2b2792b3ba1cca96a93
dohui-son <thsehgml37@korea.ac.kr>
2021-07-21 00:30:33 +0900

    review

--- greedy/B1781.cpp
+++ greedy/B1781.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-
+#include <algorithm>
 using namespace std;
 #define endl "\n";
+
+int n, d, c;
+int64_t ans;
 vector<pair<int, int> > v;
-int n, ans;
 priority_queue<int, vector<int>, greater<int> > pq;
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     cin >> n;
-    int d = 0, c = 0;
     for (int i = 0; i < n; i++)
     {
         cin >> d >> c;
@@ -30,7 +32,44 @@ int main()
             pq.pop();
         }
     }
-    cout << ans;
+    cout << ans << endl;
 
     return 0;
 }
+
+// #include <iostream>
+// #include <vector>
+// #include <queue>
+
+// using namespace std;
+// #define endl "\n";
+// vector<pair<int, int> > v;
+// int n, ans;
+// priority_queue<int, vector<int>, greater<int> > pq;
+// int main()
+// {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     cout.tie(nullptr);
+//     cin >> n;
+//     int d = 0, c = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> d >> c;
+//         v.push_back({d, c});
+//     }
+//     sort(v.begin(), v.end());
+//     for (int i = 0; i < n; i++)
+//     {
+//         pq.push(v[i].second);
+//         ans += v[i].second;
+//         if (pq.size() > v[i].first)
+//         {
+//             ans -= pq.top();
+//             pq.pop();
+//         }
+//     }
+//     cout << ans;
+
+//     return 0;
+// }
--- greedy/B9935.cpp
+++ greedy/B9935.cpp
@@ -1,29 +1,60 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <vector>
 using namespace std;
-#define endl "\n";
-string input, bomb, res;
-std::string::size_type n;
+#define endl "\n"
 
+string input, bomb, ans;
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     cin >> input >> bomb;
-
     for (int i = 0; i < input.size(); i++)
     {
-        res += input[i];
-        if (res.size() >= bomb.size() && res.substr(res.size() - bomb.size(), bomb.size()) == bomb)
+        ans += input[i];
+        if (ans.size() >= bomb.size() && ans.substr(ans.size() - bomb.size(), bomb.size()) == bomb)
         {
-            res.erase(res.end() - bomb.size(), res.end());
+
+            ans.erase(ans.end() - bomb.size(), ans.end());
         }
     }
-    if (res.size())
-        cout << res;
+    if (ans.size())
+        cout << ans << endl;
     else
-        cout << "FRULA";
+        cout << "FRULA" << endl;
 
     return 0;
 }
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+// #define endl "\n";
+// string input, bomb, res;
+// std::string::size_type n;
+
+// int main()
+// {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     cout.tie(nullptr);
+//     cin >> input >> bomb;
+
+//     for (int i = 0; i < input.size(); i++)
+//     {
+//         res += input[i];
+//         if (res.size() >= bomb.size() && res.substr(res.size() - bomb.size(), bomb.size()) == bomb)
+//         {
+//             res.erase(res.end() - bomb.size(), res.end());
+//         }
+//     }
+//     if (res.size())
+//         cout << res;
+//     else
+//         cout << "FRULA";
+
+//     return 0;
+// }
