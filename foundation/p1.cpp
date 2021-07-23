4a7fc7b8558b5b43b14d989febbf7db95289cb6f
dohui-son <thsehgml37@korea.ac.kr>
2021-07-23 18:34:31 +0900

    review

--- foundation/p1.cpp
+++ foundation/p1.cpp
@@ -23,7 +23,7 @@ void arrs(int a[][10]) { a[0][4] = 44; }
 
 struct P
 {
-    //struct with custom sorting
+    //struct with custion sorting
     int y, x;
     P(int y, int x) : y(y), x(x) {}
     P()
@@ -35,7 +35,7 @@ struct P
     {
         if (x == a.x)
             return y < a.y;
-        return x < a.x; //내림차순 x 기준
+        return x < a.x; //내림차순 -- x기준
     }
 };
 
@@ -43,137 +43,11 @@ struct percent
 {
     int x, y;
     double w, d, l;
-} sarr[6]; // cin >> sarr[0].x;
+} sarr[6]; //한정 cin>> sarr[0].x;
 
 struct Point
 {
-    //priority queue with other struct (not int)
+    //priority queue 에 int 외 다른 구조체가 들어갈때
     int y, x;
-    Point(int y, int x) : y(y), x(x) {}
-    Point()
-    {
-        y = -1;
-        x = -1;
-    }
-    bool operator<(const Point &p) const { return x > p.x; }
-    //가장 작은 수가 top (x 기준)-greater
-};
-
-struct Ppoint
-{
-    int y, x;
-};
-
-struct cmp
-{
-    bool operator()(Ppoint a, Ppoint b)
-    {
-        if (a.x == b.x)
-            return a.y <= b.y;
-        return a.x < b.x;
-    }
-};
-
-int main()
-{
-    cout << "priority q 다익스트라 그리디" << endl;
-    cout << "priority queue with struct" << endl;
-    priority_queue<Point> pq_struct;
-    pq_struct.push({1, 10});
-    pq_struct.push({2, 3});
-    pq_struct.push({4, 5});
-    pq_struct.push({6, 7});
-    cout << pq_struct.top().x << endl; //3 가장 작은 수
-    cout << pq_struct.top().y << endl; //2
-    pq_struct.pop();
-    cout << pq_struct.top().x << endl; //5
-    cout << pq_struct.top().y << endl; //4
-
-    priority_queue<Ppoint, vector<Ppoint>, cmp> pq;
-    pq.push({1, 2});
-    pq.push({10, 2});
-    pq.push({11, 1});
-    pq.push({4, 5});
-    cout << "custom pq (==default) x " << pq.top().x << endl;  // 5
-    cout << "cusotm pq (== default) y " << pq.top().y << endl; //4
-
-    priority_queue<int, vector<int>, greater<int> > pq_greater; //greater
-    pq_greater.push(2);
-    pq_greater.push(0);
-    cout << "greater -- pq.top은 가장 작은 수  " << pq_greater.top() << endl;
-    priority_queue<int> default_q;
-    default_q.push(2);
-    default_q.push(0);
-    cout << "default q - less(the biggest) " << default_q.top() << endl;
-
-    char s[50] = {
-        0,
-    };
-    string str = "good morning";
-    str += " ";
-    str += "space added";
-    cout << str << endl;
-    str = ""; //initialization
-    str = '\0';
-    cout << "str : " << str << "init" << endl;
-    int initial[10] = {
-        0,
-    };
-    for (int i : initial)
-        cout << i << " ";
-    cout << endl;
-
-    cout << "2dm array initialization" << endl;
-    int init2[10][10] = {{
-        0,
-    }};
-    cout << "2중 배열 초기화" << endl;
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
-            cout << init2[i][j] << " ";
-        }
-        cout << endl;
-    }
-    char charact[10][10] = {{
-        0,
-    }};
-    //char charact[10][10] = {{'\0',}};
-    fill(s, s + 50, 0);
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
-            cout << charact[i][j]; // initialized
-        }
-    }
-
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
-            charact[i][j] = 'i';
-            cout << charact[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    char c = '4';
-    int to_int = c - '0'; // 정수형 4
-    cout << "'4' - '0'"
-         << "int " << to_int << endl;
-    c = 'C';
-    to_int = c - 'A' + 10; //int 12
-    cout << "'C' - 'A' + 10; " << to_int << endl;
-    cout << "'A' - 'A' " << 'A' - 'A' << endl;
-    cout << " 'C' - 'A' " << 'C' - 'A' << endl;
-    c = '\0';
-    str = "123";
-    str[0]++;
-    cout << str << endl; //223
-
-    v = vector<int>(5, 0); //171번째줄 *****
-
-    return 0;
+    Point(int y, int x)
 }
--- simulation/B17406.cpp
+++ simulation/B17406.cpp
@@ -1,74 +1,79 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <tuple>
-#define endl "\n"
+#include <bits/stdc++.h>
 using namespace std;
-int n, m, k, r, c, s, ans = 987654321, tmp;
-vector<tuple<int, int, int> > kk;
-vector<vector<int> > b, a;
-int dy[] = {1, 0, -1, 0};
-int dx[] = {0, 1, 0, -1};
-
-void rot(int kdx, vector<vector<int> > &aa)
+#define s second
+#define f first
+const int INF = 987654321;
+const int dy[] = {0, 1, 0, -1};
+const int dx[] = {1, 0, -1, 0};
+int n, m, k, a[104][104], b[104][104], ret = INF, r, c, s, visited[104][104], dir, sy, sx, ey, ex;
+vector<pair<int, int> > vv;
+vector<int> v_idx;
+struct A
 {
-    tie(r, c, s) = kk[kdx]; // 돌리는 행위 s번 만큼하면 됨
-
-    int sy = r - s, sx = c - s;
-    while (s--)
-    {
-        int cnt = 4, y = sy, x = sx;
-        tmp = aa[sy][sx];
-        while (cnt < 5)
-        {
-            int ny = dy[cnt] + y;
-            int nx = dx[cnt] + x;
-            if (ny == sy && nx == sx)
-            {
-                aa[y][x] = tmp;
-                break;
-            }
-            aa[y][x] = aa[ny][nx];
-            ny = y;
-            nx = x;
-            if (ny == r - 2 || nx == c - 2 || ny == r + 2 || nx == c + 2)
-                cnt++;
-        }
-        sy++, sx++;
-    }
-}
-void BT(int start, int here, vector<vector<int> > &aa)
+    int y, x, cnt;
+};
+vector<A> v;
+// bool cmp(A & a, A & b){
+// 	if(a.y == b.y) return a.x < b.x;
+// 	return a.y < b.y;
+// }
+void go(int y, int x, int first)
 {
-
-    if (here == k)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            int tmp = 0;
-            for (int j = 0; j < m; j++)
-            {
-                tmp += aa[i][j];
-            }
-            ans = min(ans, tmp);
-        }
+    if (!first && y == sy && x == sx)
+        dir++;
+    if (!first && y == sy && x == ex)
+        dir++;
+    if (!first && y == ey && x == ex)
+        dir++;
+    if (!first && y == ey && x == sx)
+        dir++;
+    int ny = y + dy[dir];
+    int nx = x + dx[dir];
+    if (visited[ny][nx])
         return;
-    }
-
-    for (int i = start; i < k; i++)
+    visited[ny][nx] = 1;
+    vv.push_back({ny, nx});
+    go(ny, nx, 0);
+}
+void rotateAll(int y, int x, int cnt)
+{
+    for (int i = 1; i <= cnt; i++)
     {
-        b = aa;
-        BT(i + 1, here + 1, b);
-        rot(i, aa);
+        sy = y - 1 * i;
+        sx = x - 1 * i;
+        ey = y + 1 * i;
+        ex = x + 1 * i;
+        vv.clear();
+        dir = 0;
+        memset(visited, 0, sizeof(visited));
+        visited[sy][sx] = 1;
+        vv.push_back({sy, sx});
+        go(sy, sx, 1);
+        vector<int> vvv;
+        for (pair<int, int> c : vv)
+            vvv.push_back(b[c.f][c.s]);
+        rotate(vvv.begin(), vvv.begin() + vvv.size() - 1, vvv.end());
+        for (int i = 0; i < vv.size(); i++)
+            b[vv[i].f][vv[i].s] = vvv[i];
     }
 }
-
+int solve()
+{
+    for (int i : v_idx)
+        rotateAll(v[i].y, v[i].x, v[i].cnt);
+    int _ret = INF;
+    for (int i = 0; i < n; i++)
+    {
+        int cnt = 0;
+        for (int j = 0; j < m; j++)
+            cnt += b[i][j];
+        _ret = min(_ret, cnt);
+    }
+    return _ret;
+}
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
     cin >> n >> m >> k;
-    a = vector<vector<int> >(n + 1, vector<int>(m + 1, 0));
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
@@ -79,9 +84,103 @@ int main()
     for (int i = 0; i < k; i++)
     {
         cin >> r >> c >> s;
-        kk.push_back({r, c, s});
+        r--;
+        c--;
+        v.push_back({r, c, s});
+        v_idx.push_back(i);
     }
-    BT(0, 0, a);
-    cout << ans << endl;
+    do
+    {
+        memcpy(b, a, sizeof(b));
+        ret = min(ret, solve());
+    } while (next_permutation(v_idx.begin(), v_idx.end()));
+    cout << ret << "\n";
     return 0;
 }
+// #include <iostream> //디버깅 실패
+// #include <vector>
+// #include <algorithm>
+// #include <tuple>
+// #define endl "\n"
+// using namespace std;
+// int n, m, k, r, c, s, ans = 987654321, tmp;
+// vector<tuple<int, int, int> > kk;
+// vector<vector<int> > b, a;
+// int dy[] = {1, 0, -1, 0};
+// int dx[] = {0, 1, 0, -1};
+
+// void rot(int kdx, vector<vector<int> > &aa)
+// {
+//     tie(r, c, s) = kk[kdx]; // 돌리는 행위 s번 만큼하면 됨
+
+//     int sy = r - s, sx = c - s;
+//     while (s--)
+//     {
+//         int cnt = 4, y = sy, x = sx;
+//         tmp = aa[sy][sx];
+//         while (cnt < 5)
+//         {
+//             int ny = dy[cnt] + y;
+//             int nx = dx[cnt] + x;
+//             if (ny == sy && nx == sx)
+//             {
+//                 aa[y][x] = tmp;
+//                 break;
+//             }
+//             aa[y][x] = aa[ny][nx];
+//             ny = y;
+//             nx = x;
+//             if (ny == r - 2 || nx == c - 2 || ny == r + 2 || nx == c + 2)
+//                 cnt++;
+//         }
+//         sy++, sx++;
+//     }
+// }
+// void BT(int start, int here, vector<vector<int> > &aa)
+// {
+
+//     if (here == k)
+//     {
+//         for (int i = 0; i < n; i++)
+//         {
+//             int tmp = 0;
+//             for (int j = 0; j < m; j++)
+//             {
+//                 tmp += aa[i][j];
+//             }
+//             ans = min(ans, tmp);
+//         }
+//         return;
+//     }
+
+//     for (int i = start; i < k; i++)
+//     {
+//         b = aa;
+//         BT(i + 1, here + 1, b);
+//         rot(i, aa);
+//     }
+// }
+
+// int main()
+// {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     cout.tie(nullptr);
+//     cin >> n >> m >> k;
+//     a = vector<vector<int> >(n + 1, vector<int>(m + 1, 0));
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             cin >> a[i][j];
+//         }
+//     }
+//     for (int i = 0; i < k; i++)
+//     {
+//         cin >> r >> c >> s;
+//         kk.push_back({r, c, s});
+//     }
+//     BT(0, 0, a);
+//     cout << ans << endl;
+//     return 0;
+// }
