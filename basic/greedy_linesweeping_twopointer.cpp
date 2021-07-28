5b90a7839d9ae627f91bfe2fe51ae152ca6f7425
dohui-son <thsehgml37@korea.ac.kr>
2021-07-28 23:39:24 +0900

    BS

---
+++ basic/greedy_linesweeping_twopointer.cpp
@@ -0,0 +1,185 @@
+// ////B15684감시카메라
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// #define endl "\n"
+// using namespace std;
+// int n, m, a[10][10], temp[10][10], ans = 987654321;
+// vector<pair<int, int> > v;
+// const int dy[] = {-1, 0, 1, 0};
+// const int dx[] = {0, 1, 0, -1};
+
+// vector<pair<int, int> > extendCCTV(int vdx, int dir)
+// {
+//     vector<pair<int, int> > ch;
+//     int x = v[vdx].second;
+//     int y = v[vdx].first;
+
+//     switch (a[y][x])
+//     {
+//     case 1:
+//         while (1)
+//         {
+//             int ny = y + dy[dir];
+//             int nx = x + dx[dir];
+//             if (ny >= 0 && nx >= 0 && ny < n && nx < m && a[ny][nx] != 6)
+//             {
+//                 if (a[ny][nx] == 0)
+//                 {
+//                     ch.push_back({ny, nx});
+//                     a[ny][nx] = 8;
+//                 }
+//                 y = ny;
+//                 x = nx;
+//             }
+//             else
+//                 break;
+//         }
+//         break;
+//     case 2:
+//         for (int i = 0; i <= 2; i += 2)
+//         {
+//             int yy = y;
+//             int xx = x;
+//             while (1)
+//             {
+//                 int ny = yy + dy[(dir + i) % 4];
+//                 int nx = xx + dx[(dir + i) % 4];
+//                 if (ny >= 0 && nx >= 0 && ny < n && nx < m && a[ny][nx] != 6)
+//                 {
+//                     if (a[ny][nx] == 0)
+//                     {
+//                         ch.push_back({ny, nx});
+//                         a[ny][nx] = 8;
+//                     }
+//                     yy = ny;
+//                     xx = nx;
+//                 }
+//                 else
+//                     break;
+//             }
+//         }
+//         break;
+//     case 3:
+//         for (int i = 0; i < 2; i++)
+//         {
+//             int yy = y;
+//             int xx = x;
+//             while (1)
+//             {
+//                 int ny = yy + dy[(i + dir) % 4];
+//                 int nx = xx + dx[(i + dir) % 4];
+//                 if (ny >= 0 && nx >= 0 && ny < n && nx < m && a[ny][nx] != 6)
+//                 {
+//                     if (a[ny][nx] == 0)
+//                     {
+//                         ch.push_back({ny, nx});
+//                         a[ny][nx] = 8;
+//                     }
+//                     yy = ny;
+//                     xx = nx;
+//                 }
+//                 else
+//                     break;
+//             }
+//         }
+
+//         break;
+//     case 4:
+//         for (int i = 0; i < 3; i++)
+//         {
+//             int yy = y;
+//             int xx = x;
+//             while (1)
+//             {
+//                 int ny = yy + dy[(i + dir) % 4];
+//                 int nx = xx + dx[(i + dir) % 4];
+//                 if (ny >= 0 && nx >= 0 && ny < n && nx < m && a[ny][nx] != 6)
+//                 {
+//                     if (a[ny][nx] == 0)
+//                     {
+//                         ch.push_back({ny, nx});
+//                         a[ny][nx] = 8;
+//                     }
+//                     yy = ny;
+//                     xx = nx;
+//                 }
+//                 else
+//                     break;
+//             }
+//         }
+
+//         break;
+//     case 5:
+//         for (int i = 0; i < 4; i++)
+//         {
+//             int yy = y;
+//             int xx = x;
+//             while (1)
+//             {
+//                 int ny = yy + dy[(i + dir) % 4];
+//                 int nx = xx + dx[(i + dir) % 4];
+//                 if (ny >= 0 && nx >= 0 && ny < n && nx < m && a[ny][nx] != 6)
+//                 {
+//                     if (a[ny][nx] == 0)
+//                     {
+//                         ch.push_back({ny, nx});
+//                         a[ny][nx] = 8;
+//                     }
+//                     yy = ny;
+//                     xx = nx;
+//                 }
+//                 else
+//                     break;
+//             }
+//         }
+//         break;
+//     }
+
+//     return ch;
+// }
+
+// void dfs(int here)
+// {
+//     if (here == v.size())
+//     {
+//         int ret = 0;
+//         for (int i = 0; i < n; i++)
+//         {
+//             for (int j = 0; j < m; j++)
+//             {
+//                 if (a[i][j] == 0)
+//                     ret++;
+//             }
+//         }
+//         ans = min(ret, ans);
+//         return;
+//     }
+//     for (int i = 0; i < 4; i++)
+//     {
+//         vector<pair<int, int> > change = extendCCTV(here, i);
+//         dfs(here + 1);
+//         for (auto cc : change)
+//             a[cc.first][cc.second] = 0;
+//     }
+// }
+
+// int main()
+// {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     cout.tie(nullptr);
+//     cin >> n >> m;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             cin >> a[i][j];
+//             if (a[i][j] != 6 && a[i][j] != 0)
+//                 v.push_back({i, j});
+//         }
+//     }
+//     dfs(0);
+//     cout << ans << endl;
+//     return 0;
+// }
--- prefix_sum/B2632.cpp
+++ prefix_sum/B2632.cpp
@@ -1,16 +1,22 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+#define endl "\n"
 using namespace std;
 int want, n, m, temp, cnt;
 int a[1001], b[1001], ret, aSum, bSum, psum_a[2002], psum_b[2002];
 map<int, int> aCnt, bCnt;
-void make(int n, int psum[], map<int, int> &_map)
+
+void make(int n, int psum[], map<int, int> &mapp)
 {
     for (int interval = 1; interval <= n; interval++)
     {
-        for (int start = interval; start <= n + interval - 1; start++)
+        for (int start = interval; start < n + interval - 1; start++)
         {
             int sum = psum[start] - psum[start - interval];
-            _map[sum]++;
+            mapp[sum]++;
             if (interval == n)
                 break;
         }
@@ -26,23 +32,23 @@ int main()
         psum_a[i] = psum_a[i - 1] + a[i];
     }
     for (int i = n + 1; i <= 2 * n; i++)
+    {
         psum_a[i] = psum_a[i - 1] + a[i - n];
+    }
     for (int i = 1; i <= m; i++)
     {
         scanf("%d", b + i);
-        psum_b[i] = psum_b[i - 1] + b[i];
-    }
-    for (int i = m + 1; i <= 2 * m; i++)
         psum_b[i] = psum_b[i - 1] + b[i - m];
-
+    }
     make(n, psum_a, aCnt);
     make(m, psum_b, bCnt);
-
     int sum = 0;
     for (int i = 1; i < want; i++)
+    {
         sum += aCnt[want - i] * bCnt[i];
-
+    }
     sum += aCnt[want];
     sum += bCnt[want];
-    cout << sum << "\n";
+    cout << sum << endl;
+    return 0;
 }
