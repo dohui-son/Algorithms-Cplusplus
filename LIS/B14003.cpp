d15deb8273829d4d3700aa9a627173459fae7acc
dohui-son <thsehgml37@korea.ac.kr>
2021-08-05 20:43:07 +0900

    LIS

---
+++ LIS/B14003.cpp
@@ -0,0 +1,43 @@
+#include <cstdio> ///nlogn
+#include <algorithm>
+#include <stack>
+using namespace std;
+int n, lis[1000004], len, num;
+pair<int, int> ans[1000004];
+stack<int> stk;
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &num);
+        auto lowerPos = lower_bound(lis, lis + len, num);
+        int _pos = (int)(lower_bound(lis, lis + len, num) - lis);
+        if (*lowerPos == 0)
+            len++;
+        *lowerPos = num;
+        ans[i].first = _pos;
+        ans[i].second = num;
+    }
+    printf("%d\n", len);
+    //for(int i = 0; i < n; i++)printf("%d %d\n", ans[i].first, ans[i].second);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (ans[i].first == len - 1)
+        {
+            stk.push(ans[i].second);
+            len--;
+        }
+    }
+    while (stk.size())
+    {
+        printf("%d\n", stk.top());
+        stk.pop();
+    }
+    return 0;
+}
+
+/*
+8
+1 6 2 5 7 3 5 6  
+*/
---
+++ LIS/B2670.cpp
@@ -0,0 +1,30 @@
+#include <iostream> /// 그리디임
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define endl "\n"
+
+int n;
+double psum[10001], a[10001], ret, b;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    double b = a[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] > b * a[i])
+            b = a[i];
+        else
+            b *= a[i];
+        ret = max(b, ret);
+    }
+    printf("%.3f", ret + 0.00001);
+
+    return 0;
+}
