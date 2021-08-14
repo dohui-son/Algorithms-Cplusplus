e04bf3cb1cf8d698840a7569466f96ad2ec89d15
dohui-son <thsehgml37@korea.ac.kr>
2021-08-14 17:30:44 +0900

    basic algo

--- DP/B15989.cpp
+++ DP/B15989.cpp
@@ -2,22 +2,17 @@
 using namespace std;
 typedef long long ll;
 ll t, n, dp[10001];
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cin >> t;
     dp[0] = 1;
-    for (int i = 1; i <= 3; i++)
-    {
-        for (int j = 1; j <= 10000; j++)
-        {
-            if (j - i >= 0)
-                dp[j] += dp[j - i];
+    for(int i = 1; i <= 3; i++){
+        for(int j = 1; j <= 10000; j++){
+            if(j - i >= 0)dp[j] += dp[j - i];
         }
     }
-    while (t--)
-    {
+    while(t--){
         cin >> n;
         cout << dp[n] << "\n";
     }
