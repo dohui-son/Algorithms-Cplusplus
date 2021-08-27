b5f7c7b2fcf82985f0ed69c811948cbc69dc4f18
DODO <thsehgml37@korea.ac.kr>
2021-08-27 14:54:01 +0900

    dp

--- samsung/B17837.cpp
+++ samsung/B17837.cpp
@@ -1,15 +1,84 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define y aaaa
-int n, k, g[14][14], y, x, dir, ans;
+#define endl "\n"
+
+int y, x, dir, n, k, g[14][14], ans; //0은 흰색, 1은 빨간색, 2는 파란색이다.
+vector<int> m[14][14];
 struct Point
 {
     int y, x, dir;
 };
-vector<int> a[14][14];
 vector<Point> mal;
 const int dy[] = {0, 0, 0, -1, 1};
 const int dx[] = {0, 1, -1, 0, 0};
+
+int reverDir(int i)
+{
+    if (i == 1)
+        return 2;
+    if (i == 2)
+        return 1;
+    if (i == 3)
+        return 4;
+    if (i == 4)
+        return 3;
+}
+
+void movemal(int y, int x, int d, int ii)
+{
+    int ny = y + dy[d];
+    int nx = x + dx[d];
+    if (ny < 0 || ny >= n || nx < 0 || nx >= n || g[ny][nx] == 2)
+    {
+        mal[ii].dir = reverDir(d);
+        ny = y + dy[mal[ii].dir];
+        nx = x + dx[mal[ii].dir];
+        if (ny < 0 || ny >= n || nx < 0 || nx >= n || g[ny][nx] == 2)
+            return;
+    }
+    vector<int> &v = m[y][x];
+    vector<int> &nv = m[ny][nx];
+    auto pos = find(v.begin(), v.end(), ii);
+    if (g[ny][nx] == 1)
+        reverse(pos, v.end());
+
+    for (auto i = pos; i != v.end(); i++)
+    {
+        mal[*i].y = ny;
+        mal[*i].x = nx;
+        m[ny][nx].push_back(*i);
+    }
+    v.erase(pos, v.end());
+    return;
+}
+
+bool isOver()
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (m[i][j].size() >= 4)
+                return true;
+        }
+    }
+    return false;
+}
+
+bool simul()
+{
+    for (int i = 0; i < k; i++)
+    {
+        y = mal[i].y;
+        x = mal[i].x;
+        dir = mal[i].dir;
+        movemal(y, x, dir, i);
+        if (isOver())
+            return true;
+    }
+    return false;
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -23,15 +92,19 @@ int main()
             cin >> g[i][j];
         }
     }
-    int a = 0, b = 0, c = 0;
     for (int i = 0; i < k; i++)
     {
-        cin >> a >> b >> c;
-        a--, b--;
-        mal.push_back({a, b, c});
+        cin >> y >> x >> dir;
+        y--, x--;
+        mal.push_back({y, x, dir});
+        m[y][x].push_back(i);
     }
+
     while (ans <= 1000)
     {
+        ans++;
+        if (simul())
+            break;
     }
     if (ans <= 1000)
         cout << ans << endl;
