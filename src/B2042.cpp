2d1b79bde942cbbd46706573ce448c563f7fb344
DODO <thsehgml37@korea.ac.kr>
2021-09-17 12:58:31 +0900

    simulation

--- src/B2042.cpp
+++ src/B2042.cpp
@@ -1,54 +1,49 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
 using namespace std;
 typedef long long ll;
-int n, m, k, t1, t2, t3;
+int n,m,k,t1,t2,t3;
 ll t4;
-ll sum(vector<ll> &tree, int i)
-{
+ll sum(vector<ll>& tree, int i){
     ll ans = 0;
-    while (i > 0)
+    while (i>0)
     {
-        ans += tree[i];
-        i -= (i & -i);
+        ans +=tree[i];
+        i -= (i&-i);
     }
     return ans;
 }
-void update(vector<ll> &tree, int i, ll diff)
-{
-    while (i < tree.size())
+void update(vector<ll>&tree, int i, ll diff){
+    while (i<tree.size())
     {
-        tree[i] += diff;
-        i += (i & -i);
+        tree[i]+=diff;
+        i+=(i&-i);
     }
 }
 
-int main()
-{
-    scanf("%d %d %d", &n, &m, &k);
-    vector<ll> a(n + 1);
-    vector<ll> tree(n + 1);
+int main(){
+    scanf("%d %d %d", &n,&m,&k);
+    vector<ll> a(n+1);
+    vector<ll>tree(n+1);
     for (int i = 1; i <= n; i++)
     {
         scanf("%lld", &a[i]);
-        update(tree, i, a[i]);
+        update(tree,i,a[i]);
     }
-    m += k;
+    m+=k;
     while (m--)
     {
-        scanf("%d", &t1);
-        if (t1 == 1)
-        {
-            scanf("%d %lld", &t2, &t4);
+        scanf("%d",&t1);
+        if(t1 == 1){
+            scanf("%d %lld", &t2,&t4);
             ll diff = t4 - a[t2];
             a[t2] = t4;
-            update(tree, t2, diff);
+            update(tree, t2,diff);
         }
-        else
-        {
-            int i2, i3;
-            scanf("%d %d", &t2, &t3);
-            printf("%lld\n", sum(tree, t3) - sum(tree, t2 - 1));
+        else{
+            int i2,i3;
+            scanf("%d %d",&t2,&t3);
+            printf("%lld\n",sum(tree,t3)-sum(tree,t2-1));
         }
     }
     return 0;
