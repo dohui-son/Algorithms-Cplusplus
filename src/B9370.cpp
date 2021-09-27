d887cc7b3161f9d33bf0bd166ce5cad6ae8879f2
DODO <thsehgml37@korea.ac.kr>
2021-09-28 00:26:42 +0900

    dijkstra

--- src/B9370.cpp
+++ src/B9370.cpp
@@ -4,18 +4,20 @@ const int INF = 987654321;
 int T, t, n, m, s, g, h, a, b, d, x, dist[50001];
 vector<int> visited;
 
-void dijk(vector<pair<int, int> > adj[2001])
+void dijk(vector<pair<int, int> > adj[2001], vector<uint64_t> &visited)
 {
     fill(&dist[0], &dist[0] + 50001, INF);
-    priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > >
-        pq;
+    priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pq;
     pq.push({0, s});
+    visited[s] = (1 << s);
     dist[s] = 0;
     while (pq.size())
     {
         int now_d = pq.top().first;
         int here = pq.top().second;
-        if (dist[here] == now_d)
+        visited[here] |= (1 << here);
+        pq.pop();
+        if (dist[here] != now_d)
             continue;
         for (pair<int, int> next_ : adj[here])
         {
@@ -23,9 +25,8 @@ void dijk(vector<pair<int, int> > adj[2001])
             int there = next_.second;
             if (dist[there] > dist[here] + dis)
             {
-                if ((here == g && there == h) || (here == h && there == g))
-                    visited[there] = visited[here] = 1;
-                visited[there] = visited[here];
+                visited[there] |= visited[here];
+
                 dist[there] = dist[here] + dis;
                 pq.push({dist[there], there});
             }
@@ -58,12 +59,11 @@ int main()
             hoobo.push_back(x);
         }
         sort(hoobo.begin(), hoobo.end());
-        visited = vector<int>(n + 1, 0);
-        dijk(adj);
-
+        vector<uint64_t> visited(n + 1, 0);
+        dijk(adj, visited);
         for (int ho : hoobo)
         {
-            if (visited[ho])
+            if (visited[ho] & (1 << g) && visited[ho] & (1 << h) && dist[ho] != INF)
                 cout << ho << " ";
         }
         cout << "\n";
@@ -72,3 +72,47 @@ int main()
 
     return 0;
 }
+
+//숏코딩
+// #include <bits/stdc++.h>
+// #define INF 0x7fffffff-1
+// using namespace std;
+// priority_queue<pair<int,int> >pq;
+// int T,t,n,m,s,h,g,a,b,d,tmp,cur,graph[2001][2001],goal[2001],dist[2001];
+// int main(){
+// 	scanf("%d",&T);
+// 	while(T--){
+// 		for(int i=1;i<=n;i++) {
+// 			goal[i]=0;
+// 			dist[i]=0;
+// 			for(int j=1;j<=n;j++) graph[i][j]=0;
+// 		}
+// 		scanf("%d%d%d%d%d%d",&n,&m,&t,&s,&g,&h);
+// 		while(m--){
+// 			scanf("%d%d%d",&a,&b,&d);
+// 			if((a==g&&b==h)||(a==h&&b==g)) graph[a][b]=graph[b][a]=2*d-1;
+// 			else graph[a][b]=graph[b][a]=2*d;
+// 		}
+// 		while(t--){
+// 			scanf("%d",&tmp);
+// 			goal[tmp]=1;
+// 		}
+// 		for(int i=1;i<=n;i++) dist[i]=INF;
+// 		pq.push({0,s});
+// 		dist[s]=0;
+// 		while(!pq.empty()){
+// 			int distance=-pq.top().first;
+// 			int cur=pq.top().second;
+// 			pq.pop();
+// 			if(distance>dist[cur])continue;
+// 			for(int i=1;i<=n;i++){
+// 				if((dist[i]>dist[cur]+graph[i][cur])&&graph[i][cur]){
+// 					dist[i]=dist[cur]+graph[i][cur];
+// 					pq.push({-dist[i],i});
+// 				}
+// 			}
+// 		}
+// 		for(int i=1;i<=n;i++) if(goal[i]&&(dist[i]%2)) printf("%d ",i);
+// 		printf("\n");
+// 	}
+// }
